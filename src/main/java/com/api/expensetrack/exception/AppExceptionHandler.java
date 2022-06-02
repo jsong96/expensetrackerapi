c2c184736a9b489451fd935f472031b9d2d43640
Jiwon <56089838+jsong96@users.noreply.github.com>
2022-06-02 19:41:30 +0900

    update exception handler to handle internal server error

--- src/main/java/com/api/expensetrack/exception/AppExceptionHandler.java
+++ src/main/java/com/api/expensetrack/exception/AppExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.Date;
 
@@ -19,5 +20,24 @@ public class AppExceptionHandler {
         error.setMessage(e.getMessage());
         error.setTimestamp(new Date());
         return new ResponseEntity<Error>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Error> handleMethodArgumentMismatchException(MethodArgumentTypeMismatchException e, WebRequest request) {
+        Error error = new Error();
+        error.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        error.setMessage("Invalid argument type");
+        error.setTimestamp(new Date());
+        return new ResponseEntity<Error>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleGeneralException(Exception e, WebRequest request) {
+        Error error = new Error();
+        error.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.setMessage(e.getMessage());
+        error.setTimestamp(new Date());
+        return new ResponseEntity<Error>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
