03b537093aff1577f18c9094ffd67be7ef3c683d
Jiwon <56089838+jsong96@users.noreply.github.com>
2022-06-02 19:49:19 +0900

    add data validation

--- src/main/java/com/api/expensetrack/controller/ExpenseController.java
+++ src/main/java/com/api/expensetrack/controller/ExpenseController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -31,7 +32,7 @@ public class ExpenseController {
     }
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping("/expenses")
-    public Expense saveExpenseDetails(@RequestBody Expense expense) {
+    public Expense saveExpenseDetails(@Valid @RequestBody Expense expense) {
         return expenseService.saveExpenseDetails(expense);
     }
     @PutMapping("/expenses/{id}")
--- src/main/java/com/api/expensetrack/entity/Expense.java
+++ src/main/java/com/api/expensetrack/entity/Expense.java
@@ -7,6 +7,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.Date;
@@ -22,6 +24,8 @@ public class Expense {
     private Long id;
 
     @Column(name = "expense_name")
+    @NotNull(message = "expense name must not be null")
+    @Size(min = 3, message = "expense name must be at least 3 characters")
     private String name;
 
     private String description;
